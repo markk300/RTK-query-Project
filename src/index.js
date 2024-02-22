5d4454f1f8aedc44b52d95aa41866d78408ea272
iammark <mrdamarkomm@gmail.com>
2024-02-22 21:56:37 +0100

    init project with server

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApiProvider } from '@reduxjs/toolkit/query/react';
+import { apiSlice } from './feature/api/apiSlice';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ApiProvider api={apiSlice}>
     <App />
+    </ApiProvider>
   </React.StrictMode>
 );
 
